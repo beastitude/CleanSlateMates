be36ecb627a13ec8ae928faab2799a8045b97c40
ken <kendeo89@gmail.com>
2021-05-17 21:36:07 -0400

    relative file path change

--- cancellation.py
+++ cancellation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-cleanerinfo = pd.read_csv("/Users/macos/Library/Mobile Documents/com~apple~CloudDocs/python/clean slate mates/csm_cleaner info - Sheet1.csv", index_col= 0)
+cleanerinfo = pd.read_csv("csm_cleaner info - Sheet1.csv", index_col= 0)
 
 #select time arrived column
 time_col = cleanerinfo["time arrived"]
