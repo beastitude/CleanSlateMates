c7e1c132a04512e4c6680646cf2ed5081335971c
ken <kendeo89@gmail.com>
2021-05-17 22:39:41 -0400

    changed time_arrived column in csv added matplotlib to .py

--- cancellation.py
+++ cancellation.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 cleanerinfo = pd.read_csv("csm_cleaner info - Sheet1.csv", index_col= 0)
 
 #select time arrived column
-time_col = cleanerinfo["time arrived"]
+time_col = cleanerinfo["time_arrived"]
 print(time_col)
 
 #select a specific cleaner's info
 print(cleanerinfo.loc[["NX"]])
 
 #select a specific cleaner's column
-print(cleanerinfo.iloc[[2,2]])
+print(cleanerinfo.iloc[[2,1]])
+
+plt.plot(cleanerinfo.time_arrived,
+         cleanerinfo.tasks)
+plt.xlabel("time arrived")
+plt.ylabel("tasks")
+plt.show
